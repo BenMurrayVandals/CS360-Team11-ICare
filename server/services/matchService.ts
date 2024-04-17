79721318ece6f86e9d87dcff86590d3716ba05ae
Gibby <gibsonmoseley@gmail.com>
2024-04-17 14:32:41 -0700

    Cost Score Calculations

--- server/services/matchService.ts
+++ server/services/matchService.ts
@@ -4,17 +4,59 @@ export const customerAddsServiceMatchScoreCalculation = async (serviceId: string
     if(serviceType == "Lawn") {
         const service = await prisma.customerLawn.findUnique({
             where: {id: serviceId}
-         })
+        })
+        const businessServices = await prisma.businessLawn.findMany()
+        businessServices.forEach(async(businessService) => {
+            if(service.costPerMonth == 0) {
+                service.costPerMonth = 1
+            }
+            let costMatchScore = -(25/service.costPerMonth**2)*(businessService.costPerSqFoot*service.lawnSize)**2+100
+            if(costMatchScore < 0) {
+                costMatchScore = 0
+            }
+            let finalMatchScore = Math.round(((costMatchScore)/100)*100)
+            const matchEntry = await prisma.matched.create({
+                data: {customerId: service.customerId, businessId: businessService.businessId, serviceType: serviceType, cserivceId: service.id, bserviceId: businessService.id, matchScore: finalMatchScore}
+            })
+        })
     }
     else if(serviceType == "Interior") {
         const service = await prisma.customerInterior.findUnique({
             where: {id: serviceId}
          })
+        const businessServices = await prisma.businessInterior.findMany()
+        businessServices.forEach(async(businessService) => {
+            if(service.costPerMonth == 0) {
+                service.costPerMonth = 1
+            }
+            let costMatchScore = -(25/service.costPerMonth**2)*(businessService.costPerSqFoot*service.sqFootage)**2+100
+            if(costMatchScore < 0) {
+                costMatchScore = 0
+            }
+            let finalMatchScore = Math.round(((costMatchScore)/100)*100)
+            const matchEntry = await prisma.matched.create({
+                data: {customerId: service.customerId, businessId: businessService.businessId, serviceType: serviceType, cserivceId: service.id, bserviceId: businessService.id, matchScore: finalMatchScore}
+            })
+        })
     }
     else if(serviceType == "Morgage") {
         const service = await prisma.customerMorgage.findUnique({
             where: {id: serviceId}
-         })
+        })
+        const businessServices = await prisma.businessMorgage.findMany()
+        businessServices.forEach(async(businessService) => {
+            if(service.costPerMonth == 0) {
+                service.costPerMonth = 1
+            }
+            let costMatchScore = -(25/service.costPerMonth**2)*(businessService.insuranceRate)**2+100
+            if(costMatchScore < 0) {
+                costMatchScore = 0
+            }
+            let finalMatchScore = Math.round(((costMatchScore)/100)*100)
+            const matchEntry = await prisma.matched.create({
+                data: {customerId: service.customerId, businessId: businessService.businessId, serviceType: serviceType, cserivceId: service.id, bserviceId: businessService.id, matchScore: finalMatchScore}
+            })
+        })
     }
     else if(serviceType == "Insurance") {
         const service = await prisma.customerInsurance.findUnique({
