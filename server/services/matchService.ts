0428d6e5ebfa299f3197d81f97225eca56120101
Gibby <gibsonmoseley@gmail.com>
2024-04-24 11:08:58 -0700

    Fixed My math

--- server/services/matchService.ts
+++ server/services/matchService.ts
@@ -94,8 +94,11 @@ export const customerAddsServiceMatchScoreCalculation = async (serviceId: string
       if (service.totalCoverage == 0) {
         service.totalCoverage = 1;
       }
-      let totalCoverageScore = -(25 / service.totalCoverage ** 2) * (businessService.totalCoverage) ** 2 + 100;
-      if (totalCoverageScore < 0) {
+      let totalCoverageScore = (-25/service.totalCoverage**2)*(businessService.totalCoverage-(2*service.totalCoverage))**2+100;
+      if (totalCoverageScore > 100) {
+        totalCoverageScore = 100;
+      }
+      else if(totalCoverageScore < 0) {
         totalCoverageScore = 0;
       }
       let finalMatchScore = Math.round((costMatchScore+totalCoverageScore / 200) * 100);
@@ -121,10 +124,11 @@ export const customerAddsServiceMatchScoreCalculation = async (serviceId: string
         service.costPerMonth = 1;
       }
       let costMatchScore = -(25 / service.costPerMonth ** 2) * businessService.costPerMonth ** 2 + 100;
-      let speedMatchScore;
-      if (businessService.speed > service.speed) {
-        let speedMatchScore = (service.speed / businessService.speed) * 100;
-      } else {
+      let speedMatchScore = (-25/service.speed**2)*(businessService.speed-(2*service.speed))**2+100
+      if (speedMatchScore > 100) {
+          speedMatchScore = 100;
+      }
+      else if ((speedMatchScore < 0))  {
         let speedMatchScore = 0;
       }
       if (costMatchScore < 0) {
@@ -156,7 +160,14 @@ export const customerAddsServiceMatchScoreCalculation = async (serviceId: string
       if (costMatchScore < 0) {
         costMatchScore = 0;
       }
-      let finalMatchScore = Math.round((costMatchScore / 100) * 100);
+      let GBMatchScore = (-25/service.GBPerMonth**2)*(businessService.GBPerMonth-(2*service.GBPerMonth))**2+100
+      if (GBMatchScore > 100) {
+        GBMatchScore = 100;
+      }
+      else if (GBMatchScore < 0) {
+        GBMatchScore = 0;
+      } 
+      let finalMatchScore = Math.round((costMatchScore+GBMatchScore / 200) * 100);
       const matchEntry = await prisma.matched.create({
         data: {
           customerId: service.customerId,
@@ -266,8 +277,11 @@ export const businessAddsServiceMatchScoreCalculation = async (businessId: strin
       if (customerService.totalCoverage == 0) {
         customerService.totalCoverage = 1;
       }
-      let totalCoverageScore = -(25 / customerService.totalCoverage ** 2) * (business.totalCoverage) ** 2 + 100;
-      if (totalCoverageScore < 0) {
+      let totalCoverageScore = (-25/customerService.totalCoverage**2)*(business.totalCoverage-(2*customerService.totalCoverage))**2+100;
+      if (totalCoverageScore > 100) {
+        totalCoverageScore = 100;
+      }
+      else if(totalCoverageScore < 0) {
         totalCoverageScore = 0;
       }
       let finalMatchScore = Math.round((costMatchScore+totalCoverageScore / 200) * 100);
@@ -292,10 +306,17 @@ export const businessAddsServiceMatchScoreCalculation = async (businessId: strin
         customerService.costPerMonth = 1;
       }
       let costMatchScore = -(25 / customerService.costPerMonth ** 2) * business.costPerMonth ** 2 + 100;
+      let speedMatchScore = (-25/customerService.speed**2)*(business.speed-(2*customerService.speed))**2+100
+      if (speedMatchScore > 100) {
+          speedMatchScore = 100;
+      }
+      else if ((speedMatchScore < 0))  {
+        let speedMatchScore = 0;
+      }
       if (costMatchScore < 0) {
         costMatchScore = 0;
       }
-      let finalMatchScore = Math.round((costMatchScore / 100) * 100);
+      let finalMatchScore = Math.round(((costMatchScore + speedMatchScore) / 200) * 100);
       const matchEntry = await prisma.matched.create({
         data: {
           customerId: customerService.customerId,
@@ -321,7 +342,14 @@ export const businessAddsServiceMatchScoreCalculation = async (businessId: strin
       if (costMatchScore < 0) {
         costMatchScore = 0;
       }
-      let finalMatchScore = Math.round((costMatchScore / 100) * 100);
+      let GBMatchScore = (-25/customerService.GBPerMonth**2)*(business.GBPerMonth-(2*customerService.GBPerMonth))**2+100
+      if (GBMatchScore > 100) {
+        GBMatchScore = 100;
+      }
+      else if (GBMatchScore < 0) {
+        GBMatchScore = 0;
+      } 
+      let finalMatchScore = Math.round((costMatchScore+GBMatchScore / 200) * 100);
       const matchEntry = await prisma.matched.create({
         data: {
           customerId: customerService.customerId,
@@ -410,57 +438,64 @@ export const editMatchScoreCustomer = async (serviceId: string, serviceType: str
             })
         })
     }
-    else if(serviceType == "Insurance") {
-        const service = await prisma.customerInsurance.findUnique({
-            where: {id: serviceId}
-         })
-        const businessServices = await prisma.businessInsurance.findMany()
-        businessServices.forEach(async(businessService) => {
-            if(service.costPerMonth == 0) {
-                service.costPerMonth = 1
-            }
-            let costMatchScore = -(25/service.costPerMonth**2)*(businessService.costPerSqFoot*service.sqFootage)**2+100
-            if(costMatchScore < 0) {
-                costMatchScore = 0
-            }
-            if(service.totalCoverage == 0) {
-              service.totalCoverage = 1
-            }
-            let totalCoverageScore = -(25 / service.totalCoverage ** 2) * (businessService.totalCoverage) ** 2 + 100;
-            if (totalCoverageScore < 0) {
-              totalCoverageScore = 0;
-            }
-            let finalMatchScore = Math.round((costMatchScore+totalCoverageScore / 200) * 100);
-            const matchEntry = await prisma.matched.updateMany({
-                where: {
-                    cserivceId: service.id,
-                    bserviceId: businessService.id
-                },
-                data: { matchScore: finalMatchScore},
-            })
+    else if (serviceType == "Insurance") {
+      const service = await prisma.customerInsurance.findUnique({
+        where: { id: serviceId },
+      });
+      const businessServices = await prisma.businessInsurance.findMany();
+      businessServices.forEach(async (businessService) => {
+        if (service.costPerMonth == 0) {
+          service.costPerMonth = 1;
+        }
+        let costMatchScore = -(25 / service.costPerMonth ** 2) * (businessService.costPerSqFoot * service.sqFootage) ** 2 + 100;
+        if (costMatchScore < 0) {
+          costMatchScore = 0;
+        }
+        if (service.totalCoverage == 0) {
+          service.totalCoverage = 1;
+        }
+        let totalCoverageScore = (-25/service.totalCoverage**2)*(businessService.totalCoverage-(2*service.totalCoverage))**2+100;
+        if (totalCoverageScore > 100) {
+          totalCoverageScore = 100;
+        }
+        else if(totalCoverageScore < 0) {
+          totalCoverageScore = 0;
+        }
+        if (totalCoverageScore < 0) {
+          totalCoverageScore = 0;
+        }
+        let finalMatchScore = Math.round((costMatchScore+totalCoverageScore / 200) * 100);
+        const matchEntry = await prisma.matched.updateMany({
+          where: {
+              cserivceId: service.id,
+              bserviceId: businessService.id
+          },
+          data: { matchScore: finalMatchScore},
         })
+      });
     }
     else if(serviceType == "Internet") {
-        const service = await prisma.customerInternet.findUnique({
-            where: {id: serviceId}
-         })
-        const businessServices = await prisma.businessInternet.findMany()
-        businessServices.forEach(async(businessService) => {
-            //-(25/customervalue^2)(buisnesscost^2)+100
-            if(service.costPerMonth == 0) {
-                service.costPerMonth = 1
-            }
-            let costMatchScore = -(25/service.costPerMonth**2)*(businessService.costPerMonth)**2+100
-            let speedMatchScore
-            if(businessService.speed > service.speed) {
-                let speedMatchScore = (service.speed/businessService.speed)*100
-            } else {
-                let speedMatchScore = 0;
-            }
-            if(costMatchScore < 0) {
-                costMatchScore = 0
-            }
-            let finalMatchScore = Math.round(((costMatchScore+speedMatchScore)/200)*100)
+      const service = await prisma.customerInternet.findUnique({
+        where: { id: serviceId },
+      });
+      const businessServices = await prisma.businessInternet.findMany();
+      businessServices.forEach(async (businessService) => {
+        //-(25/customervalue^2)(buisnesscost^2)+100
+        if (service.costPerMonth == 0) {
+          service.costPerMonth = 1;
+        }
+        let costMatchScore = -(25 / service.costPerMonth ** 2) * businessService.costPerMonth ** 2 + 100;
+        let speedMatchScore = (-25/service.speed**2)*(businessService.speed-(2*service.speed))**2+100
+        if (speedMatchScore > 100) {
+            speedMatchScore = 100;
+        }
+        else if ((speedMatchScore < 0))  {
+          let speedMatchScore = 0;
+        }
+        if (costMatchScore < 0) {
+          costMatchScore = 0;
+        }
+        let finalMatchScore = Math.round(((costMatchScore + speedMatchScore) / 200) * 100);
             const matchEntry = await prisma.matched.updateMany({
                 where: {
                     cserivceId: service.id,
@@ -484,7 +519,16 @@ export const editMatchScoreCustomer = async (serviceId: string, serviceType: str
             if (costMatchScore < 0) {
                 costMatchScore = 0;
             }
-            let finalMatchScore = Math.round((costMatchScore / 100) * 100);
+            let speedMatchScore = 0;
+            if (businessService.GBPerMonth > service.GBPerMonth) {
+              let speedMatchScore = (-25/service.GBPerMonth**2)*(businessService.GBPerMonth-(2*service.GBPerMonth))**2+100
+              if (speedMatchScore > 100) {
+                speedMatchScore = 100;
+              }
+            } else {
+              let speedMatchScore = 0;
+            }
+            let finalMatchScore = Math.round((costMatchScore+speedMatchScore / 200) * 100);
             const matchEntry = await prisma.matched.updateMany({
                 where: {
                     cserivceId: service.id,
@@ -592,8 +636,11 @@ export const editMatchScoreBuisness = async (businessId: string, businessType: s
             if (customerService.totalCoverage == 0) {
               customerService.totalCoverage = 1;
             }
-            let totalCoverageScore = -(25 / customerService.totalCoverage ** 2) * (business.totalCoverage) ** 2 + 100;
-            if (totalCoverageScore < 0) {
+            let totalCoverageScore = (-25/customerService.totalCoverage**2)*(business.totalCoverage-(2*customerService.totalCoverage))**2+100;
+            if (totalCoverageScore > 100) {
+              totalCoverageScore = 100;
+            }
+            else if(totalCoverageScore < 0) {
               totalCoverageScore = 0;
             }
             let finalMatchScore = Math.round((costMatchScore+totalCoverageScore / 200) * 100);
@@ -607,19 +654,26 @@ export const editMatchScoreBuisness = async (businessId: string, businessType: s
         })
     }
     else if(businessType == "Internet") {
-        const business = await prisma.businessInternet.findUnique({
-            where: {id: businessId}
-        })
-        const customerServices = await prisma.customerInternet.findMany()
-        customerServices.forEach(async(customerService) => {
-            if(customerService.costPerMonth == 0) {
-                customerService.costPerMonth = 1
-            }
-            let costMatchScore = -(25/customerService.costPerMonth**2)*(business.costPerMonth)**2+100
-            if(costMatchScore < 0) {
-                costMatchScore = 0
-            }
-            let finalMatchScore = Math.round(((costMatchScore)/100)*100)
+      const business = await prisma.businessInternet.findUnique({
+        where: { id: businessId },
+      });
+      const customerServices = await prisma.customerInternet.findMany();
+      customerServices.forEach(async (customerService) => {
+        if (customerService.costPerMonth == 0) {
+          customerService.costPerMonth = 1;
+        }
+        let costMatchScore = -(25 / customerService.costPerMonth ** 2) * business.costPerMonth ** 2 + 100;
+        let speedMatchScore = (-25/customerService.speed**2)*(business.speed-(2*customerService.speed))**2+100
+        if (speedMatchScore > 100) {
+            speedMatchScore = 100;
+        }
+        else if ((speedMatchScore < 0))  {
+          let speedMatchScore = 0;
+        }
+        if (costMatchScore < 0) {
+          costMatchScore = 0;
+        }
+        let finalMatchScore = Math.round(((costMatchScore + speedMatchScore) / 200) * 100);
             const matchEntry = await prisma.matched.updateMany({
                 where: {
                     cserivceId: customerService.id,
@@ -635,21 +689,28 @@ export const editMatchScoreBuisness = async (businessId: string, businessType: s
         })
         const customerServices = await prisma.customerCell.findMany()
         customerServices.forEach(async(customerService) => {
-            if(customerService.costPerMonth == 0) {
-                customerService.costPerMonth = 1
-            }
-            let costMatchScore = -(25/customerService.costPerMonth**2)*(business.costPerMonth)**2+100
-            if(costMatchScore < 0) {
-                costMatchScore = 0
-            }
-            let finalMatchScore = Math.round(((costMatchScore)/100)*100)
-            const matchEntry = await prisma.matched.updateMany({
-                where: {
-                    cserivceId: customerService.id,
-                    bserviceId: business.id
-                },
-                data: { matchScore: finalMatchScore},
-            })
+          if (customerService.costPerMonth == 0) {
+            customerService.costPerMonth = 1;
+          }
+          let costMatchScore = -(25 / customerService.costPerMonth ** 2) * business.costPerMonth ** 2 + 100;
+          if (costMatchScore < 0) {
+            costMatchScore = 0;
+          }
+          let GBMatchScore = (-25/customerService.GBPerMonth**2)*(business.GBPerMonth-(2*customerService.GBPerMonth))**2+100
+          if (GBMatchScore > 100) {
+            GBMatchScore = 100;
+          }
+          else if (GBMatchScore < 0) {
+            GBMatchScore = 0;
+          } 
+          let finalMatchScore = Math.round((costMatchScore+GBMatchScore / 200) * 100);
+          const matchEntry = await prisma.matched.updateMany({
+            where: {
+              cserivceId: customerService.id,
+              bserviceId: business.id
+            },
+            data: { matchScore: finalMatchScore},
+          })
         })
     }
     else {
